3f2815eb881785c1896da499defb8e4422221d8b
Tommy <tommy.stanley96@gmail.com>
2016-11-12 21:05:03 -0600

    Start of loading curve files

--- Sources/main.swift
+++ Sources/main.swift
@@ -1,2 +1,37 @@
-print("suck my dick")
+import Foundation
 
+let path = "Resources/teapot"        
+
+let fileContents : String
+
+do
+{
+    fileContents = try String(contentsOfFile: path, encoding: String.Encoding.utf8)
+}
+catch
+{
+    print("Error reading ")
+    fileContents = ""
+}
+
+let lines = fileContents.components(separatedBy: "\n")
+
+let numPatches = Int(lines[0])!
+let numVectors = Int(lines[numPatches + 1])!
+print("Loading curve file with \(numPatches) patches and \(numVectors) vectors")
+
+for i in 0 ..< numVectors
+{
+    let curLine = lines[numPatches + 2 + i].components(separatedBy: ",")
+    
+    let x = Double(curLine[0])!
+    let y = Double(curLine[1])!
+    let z = Double(curLine[2])!
+    
+    print("<\(x),\(y),\(z)>")
+}
+
+for i in 0 ..< numPatches
+{
+    print(i)
+}
